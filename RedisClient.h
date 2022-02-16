fc00954d73479a9e95c3e13efc69032affaccdcb
XinShuoWang <56379080+XinShuoWang@users.noreply.github.com>
2022-02-16 15:15:08 +0800

    Create RedisClient.h

---
+++ RedisClient.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include "hiredis.h"
+
+class RedisClient
+{
+public:
+    RedisClient();
+    RedisClient(std::string host, int port);
+    ~RedisClient();
+    void connect(std::string, int);
+    bool set(const char *, size_t, const char *, size_t);
+    std::shared_ptr<redisReply> get(char *, size_t);
+
+private:
+    redisContext * context;
+};
